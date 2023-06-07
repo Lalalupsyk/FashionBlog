b9790d25d0f300dc06bd0019e3db3be804c31619
Khrystyna <m.christinaaa>
2023-06-07 20:02:55 +0300

    first commit

---
+++ src/components/HeaderSlide.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "HeaderSlide"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/PostsPage.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PostsPage"
+}
+</script>
+
+<style scoped>
+
+</style>
